b89dfad508bbea65dcd14554da701505365d984a
zhangcheng <776331229@qq.com>
2017-10-17 10:18:52 +0800

    修正 三级类目选择插件

--- js/jquery.multiSelect.js
+++ js/jquery.multiSelect.js
@@ -99,34 +99,39 @@
                         },
                     ]
                 },
-            ]
+            ],
+            firstIndex:0, // 初始化第一列选中的index
+            secondIndex:0, // 初始化第二列选中的index
+            thirdIndex:0 // 初始化第三列选中的index
         };
         this.options = $.extend({},defaults,options);
-        this.html = '<div class="x-down-select-box"">'+
+        this.html = '<div class="x-down-select-box">'+
                         '<ul id="first-select-list" class="x-select-list"></ul>'+
                         '<ul id="second-select-list" class="x-select-list"></ul>'+
                         '<ul id="third-select-list" class="x-select-list"></ul>'+
                     '</div>';
-    }
+    };
 
     select.prototype = {
         /**
          * 初始化选择列表
          * */
         init: function(){
-            $('body').append(this.html);
-            this.bindEvent();
-            this.updateData();
-            setTimeout(function(){
-                $('.x-down-select-box').addClass('-show');
-            },10);
+            if($('.x-down-select-box').length === 0){
+                $('body').append(this.html);
+                this.bindEvent();
+                this.updateData();
+                setTimeout(function(){
+                    $('.x-down-select-box').addClass('-show');
+                },10);
+            }
         },
         /**
          * 更新数据
          * */
         updateData: function(){
-            var posTop =  this.$el.offsetTop + this.$el.offsetHeight + 2, // 位置top
-                posLeft = this.$el.offsetLeft; // 位置left
+            var posTop =  $(this.$el).offset().top + this.$el.offsetHeight + 2, // 位置top
+                posLeft = $(this.$el).offset().left; // 位置left
 
             this.initFirstSelectList();
             $('.x-down-select-box').css('top',posTop);
@@ -141,8 +146,12 @@
             for(var i = 0;i < list.length;i++){
                 var firstHtml = '';
                 if(list[i].childs.length > 0){
-                    this.initSecondSelectList(0);
-                    firstHtml = `<li data-type="mouse-over-li" data-id=${list[i].id} data-column="1" data-i=${i}>${list[i].name}</li>`;
+                    if(i === this.options.firstIndex){
+                        firstHtml = `<li class="-active" data-type="mouse-over-li" data-id=${list[i].id} data-column="1" data-i=${i}>${list[i].name}</li>`;
+                    } else {
+                        firstHtml = `<li data-type="mouse-over-li" data-id=${list[i].id} data-column="1" data-i=${i}>${list[i].name}</li>`;
+                    }
+
                 } else {
                     firstHtml = `<li class="-no-child" data-type="mouse-over-li" data-id=${list[i].id} data-column="1" data-i=${i}>${list[i].name}</li>`;
                     $('#second-select-list').html('');
@@ -150,6 +159,7 @@
                 firstListHtmlArry.push(firstHtml);
             }
             $('#first-select-list').html(firstListHtmlArry.join(''));
+            this.initSecondSelectList(this.options.firstIndex);
         },
         /**
          * 初始化第二个列表
@@ -162,18 +172,25 @@
             for(var j = 0;j < list[i].childs.length;j++){
                 var secondHtml = '';
                 if(list[i].childs[j].childs.length > 0){
-                    if(i === 0){ // 初始化刚打开时候
-                        this.initThirdSelectList(0,0);
+                    if(j === this.options.secondIndex){
+                        secondHtml = `<li class="-active" data-type="mouse-over-li" data-id=${list[i].childs[j].id} data-column="2" data-i=${i} data-j=${j}>${list[i].childs[j].name}</li>`;
                     } else {
-                        this.initThirdSelectList(i,j);
+                        secondHtml = `<li data-type="mouse-over-li" data-id=${list[i].childs[j].id} data-column="2" data-i=${i} data-j=${j}>${list[i].childs[j].name}</li>`;
                     }
-                    secondHtml = `<li data-type="mouse-over-li" data-id=${list[i].childs[j].id} data-column="2" data-i=${i} data-j=${j}>${list[i].childs[j].name}</li>`;
                 } else {
                     secondHtml = `<li class="-no-child" data-type="mouse-over-li" data-id=${list[i].childs[j].id} data-column="2" data-i=${i} data-j=${j}>${list[i].childs[j].name}</li>`;
                     $('#third-select-list').html('');
                 }
                 secondListHtmlArry.push(secondHtml);
             }
+
+            // 之前设置过后，则选中！但是有种情况是之前选中的第二级目录的index 会大于原数组
+            if(this.options.secondIndex < j){
+                this.initThirdSelectList(i,this.options.secondIndex);
+            } else {
+                this.initThirdSelectList(i,0);
+            }
+
             $('#second-select-list').html(secondListHtmlArry.join(''));
         },
         /**
@@ -183,9 +200,15 @@
          * */
         initThirdSelectList: function(i,j){
             var list = this.options.list,
-                thirdListHtmlArry = [];
+                thirdListHtmlArry = [],
+                thirdHtml = '';
+
             for(var k = 0;k < list[i].childs[j].childs.length;k++){
-                var thirdHtml = `<li class="-no-child" data-type="mouse-over-li" data-id=${list[i].childs[j].childs[k].id} data-column="3" data-i=${i} data-j=${j} data-k=${k}>${list[i].childs[j].childs[k].name}</li>`;
+                if(k === this.options.thirdIndex){
+                    thirdHtml = `<li class="-no-child -active" data-type="mouse-over-li" data-id=${list[i].childs[j].childs[k].id} data-column="3" data-i=${i} data-j=${j} data-k=${k}>${list[i].childs[j].childs[k].name}</li>`;
+                } else {
+                    thirdHtml = `<li class="-no-child" data-type="mouse-over-li" data-id=${list[i].childs[j].childs[k].id} data-column="3" data-i=${i} data-j=${j} data-k=${k}>${list[i].childs[j].childs[k].name}</li>`;
+                }
                 thirdListHtmlArry.push(thirdHtml);
             }
             $('#third-select-list').html(thirdListHtmlArry.join(''));
@@ -205,13 +228,12 @@
          * */
         bindEvent: function () {
             var that = this;
-            //
+            // 点击其它地方，隐藏下拉列表
             setTimeout(function(){
                 $(document).on('click',function(){
                     that.destroy();
                 });
-            },10);
-
+            },100);
 
             // 根据data-type 绑定对应的事件
             $('.x-down-select-box').on({
@@ -275,7 +297,7 @@
                     };
 
                     // 点击其它地方时候隐藏下拉框
-                    this.stopPropagation(event);
+                    that.stopPropagation(event);
                 },
             });
         },
@@ -297,26 +319,38 @@
          * 鼠标移上去的选择时间
          * */
         mouseOverSelect: function(event){
-            var listColumn= event.target.getAttribute("data-column"), // 当前是第几个分类
+            var listColumn= event.target.getAttribute("data-column")*1, // 当前是第几个分类
                 i= event.target.getAttribute("data-i"), // 当前选择的是属于第一分类中的第几个
                 j= event.target.getAttribute("data-j"); // 当前选择的是属于第二分类中的第几个
             if(!this.mouseOver){
                 this.mouseOver = true;
 
-                if(listColumn == 1){
-                    $('#first-select-list li').removeClass('-active'); // 把第一列的选中li 取出-active
-                    // 添加选中样式
-                    if(event.target.className.indexOf('-active') === -1){
-                        event.target.className += ' -active';
-                    }
-                    this.initSecondSelectList(i);
-                } else if(listColumn == 2){
-                    $('#second-select-list li').removeClass('-active'); // 把第二列的选中li 取出-active
-                    // 添加选中样式
-                    if(event.target.className.indexOf('-active') === -1){
-                        event.target.className += ' -active';
-                    }
-                    this.initThirdSelectList(i,j);
+                switch (listColumn){
+                    case 1:
+                        $('#first-select-list li').removeClass('-active'); // 把第一列的选中li 取出-active
+                        // 添加选中样式
+                        if(event.target.className.indexOf('-active') === -1){
+                            event.target.className += ' -active';
+                        }
+                        this.initSecondSelectList(i);
+                        break;
+                    case 2:
+                        $('#second-select-list li').removeClass('-active'); // 把第二列的选中li 取出-active
+                        // 添加选中样式
+                        if(event.target.className.indexOf('-active') === -1){
+                            event.target.className += ' -active';
+                        }
+                        this.initThirdSelectList(i,j);
+                        break;
+                    case 3:
+                        $('#third-select-list li').removeClass('-active'); // 把第二列的选中li 取出-active
+                        // 添加选中样式
+                        if(event.target.className.indexOf('-active') === -1){
+                            event.target.className += ' -active';
+                        }
+                        break;
+                    default :
+                        break;
                 }
             }
         },
